c28573ac24c8d4180a11f4310a5ec73a9b207916
SiYeon.Kim <kciy99@naver.com>
2021-11-16 10:59:10 +0900

    change main to use list ops

--- git_practice/main.py
+++ git_practice/main.py
@@ -1,6 +1,9 @@
-from add_lists import add_lists
+import list_ops as lo
 
 if __name__ == "__main__":
-    result = add_lists(['wel', 't', 'r'], ['come', 'o', 'os'])
-    print(result)
-
+    foo = [1,2,3]
+    bar = [4,5,6]
+    print("foo+bar=", lo.add(foo, bar))
+    print("foo-bar=", lo.subtract(foo, bar))
+    print("foo*bar=", lo.multiply(foo, bar))
+    print("foo/bar=", lo.divide(foo, bar))
